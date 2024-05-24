011a120dfc6aca7430b506becb69eaff9f8af065
shiva-art <shivakasi.karnati@gmail.com>
2024-05-24 12:24:29 +0530

    OS command injection by removing the lower case conversion

--- sql_injection_api.py
+++ sql_injection_api.py
@@ -13,27 +13,12 @@ tfidf_vectorizer = joblib.load("tfidf_vectorizer_OS.pkl")
 model = joblib.load("trained_model_OS.pkl")
 
 @app.route('/', methods=['POST'])
-def detect_sql_injection_api():
+def detect_OS_injection_api():
     username = request.json.get('username')
     password = request.json.get('password')
 
-    # Regular expression for detecting SQL injection patterns
-    #sql_pattern = re.compile(r".*(\'|\"|;|--|union|truncate|-).*")
-
-    # Check for SQL injection patterns in the username and password
-    #username_is_sql_injection = bool(sql_pattern.match(username))
-    #password_is_sql_injection = bool(sql_pattern.match(password))
-
-    # If SQL injection is detected, return appropriate response
-    #if username_is_sql_injection or password_is_sql_injection:
-    #    return jsonify({
-    #        "username_is_sql_injection": username_is_sql_injection,
-    #        "password_is_sql_injection": password_is_sql_injection,
-    #        "message": "SQL injection detected"
-    #    })
-
     # If no SQL injection patterns are found, use the trained model
-    query = tfidf_vectorizer.transform([username.lower(), password.lower()])
+    query = tfidf_vectorizer.transform([username, password ])
     predictions = model.predict(query)
 
     return jsonify({
